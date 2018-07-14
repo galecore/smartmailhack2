de15aec55db1bb84d7fca6a8f20e58d429807f00
Tima Shunin <tima.shunin@gmail.com>
2018-07-14 20:34:50 +0300

    Directories restructured. Server code rewritten. Static and templates folder added.

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    letters = list()
+    with open("mailparser/letters.json", 'r') as letters_file:
+        letters = json.load(letters_file)
+    return render_template("index.html", letters=letters)
 
 if __name__ == "__main__":
-	app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
