507f31f7eccedaa6607fbd1d60b0618b578c2856
Tima Shunin <tima.shunin@gmail.com>
2018-07-14 21:04:30 +0300

    Fixed index.html. Added file uploads.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import json
 
 app = Flask(__name__)
@@ -10,5 +10,9 @@ def hello():
         letters = json.load(letters_file)
     return render_template("index.html", letters=letters)
 
+@app.route("/data/<filename>")
+def upload(filename):
+    return send_from_directory("/mailparser", filename)
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
